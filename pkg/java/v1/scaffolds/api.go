d68027fb7fa12bdf47a7fb48e200d4fd45b7225c
laxmikantbpandhare <laxmikantpandhare@gmail.com>
2021-03-23 18:38:45 -0700

    added docker file and pom file

--- pkg/java/v1/scaffolds/api.go
+++ pkg/java/v1/scaffolds/api.go
@@ -1,4 +1,4 @@
-package scaffolds
+	package scaffolds
 
 import (
 	"fmt"
---
+++ pkg/java/v1/scaffolds/internal/templates/dockerfile.go
@@ -0,0 +1,32 @@
+package templates
+
+import "sigs.k8s.io/kubebuilder/v3/pkg/model/file"
+
+var _ file.Template = &DockerFile{}
+
+type DockerFile struct {
+	file.TemplateMixin
+}
+
+func (f *DockerFile) SetTemplateDefaults() error {
+	if f.Path == "" {
+		f.Path = "Dockerfile"
+	}
+
+	f.TemplateBody = dockerfileTemplate
+
+	return nil
+}
+
+// TODO: pass in the name of the operator i.e. replace Memcached
+const dockerfileTemplate = `FROM quay.io/operator-framework/java-operator:{{ .JavaOperatorVersion }}
+
+COPY requirements.yml ${HOME}/requirements.yml
+RUN ansible-galaxy collection install -r ${HOME}/requirements.yml \
+&& chmod -R ug+rwx ${HOME}/.ansible
+
+COPY watches.yaml ${HOME}/watches.yaml
+COPY {{ .RolesDir }}/ ${HOME}/{{ .RolesDir }}/
+COPY {{ .PlaybooksDir }}/ ${HOME}/{{ .PlaybooksDir }}/
+`
+

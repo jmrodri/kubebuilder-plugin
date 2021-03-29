0d74730cc3d62fc8997aa6630b2cc1785181604a
laxmikantbpandhare <laxmikantpandhare@gmail.com>
2021-03-28 17:27:40 -0700

    modified files according to beta version

--- pkg/java/v1/scaffolds/api.go
+++ pkg/java/v1/scaffolds/api.go
@@ -2,18 +2,31 @@ package scaffolds
 
 import (
 	"fmt"
+	"sigs.k8s.io/kubebuilder/v3/pkg/config"
+	"sigs.k8s.io/kubebuilder/v3/pkg/model/resource"
+	"sigs.k8s.io/kubebuilder/v3/pkg/plugins"
 
 	"sigs.k8s.io/kubebuilder/v3/pkg/machinery"
 
-	"github.com/java-operator-sdk/kubebuilder-plugin/pkg/internal/kubebuilder/cmdutil"
 )
 
 type apiScaffolder struct {
 	fs machinery.Filesystem
+
+	config   config.Config
+	resource resource.Resource
+
+	doRole, doPlaybook bool
 }
 
-func NewCreateAPIScaffolder() cmdutil.Scaffolder {
-	return &apiScaffolder{}
+// NewCreateAPIScaffolder returns a new plugins.Scaffolder for project initialization operations
+func NewCreateAPIScaffolder(cfg config.Config, res resource.Resource, doRole, doPlaybook bool) plugins.Scaffolder {
+	return &apiScaffolder{
+		config:     cfg,
+		resource:   res,
+		doRole:     doRole,
+		doPlaybook: doPlaybook,
+	}
 }
 
 func (s *apiScaffolder) InjectFS(fs machinery.Filesystem) {
